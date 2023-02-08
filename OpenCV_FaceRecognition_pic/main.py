e9dbbfabcb846834174d88ec0e36299b88623843
Gulyutin <gulyutin@gmail.com>
2023-02-08 16:02:10 +0700

    Face recognition done

--- OpenCV_FaceRecognition_pic/main.py
+++ OpenCV_FaceRecognition_pic/main.py
@@ -2,10 +2,11 @@ import cv2
 
 face_cascade_db = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
 
-img = cv2.imread("test2.jpg")
+img = cv2.imread("OpenCV_FaceRecognition_pic/test2.jpg")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade_db.detectMultiScale(img_gray, 1.1, 19)
+
 for (x,y,w,h) in faces: 
     cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
 
