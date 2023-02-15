91a44aaeeb8f3253a9d2d7546d9fed210deef9f7
Gulyutin <gulyutin@gmail.com>
2023-02-15 17:22:49 +0700

    Webcam Eye recognition from webcam video stream updated

--- OpenCV_EyeRecognition_webcam/main.py
+++ OpenCV_EyeRecognition_webcam/main.py
@@ -7,9 +7,7 @@ cap = cv2.VideoCapture(0)
 
 while True: 
     success, img = cap.read()
-
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
     faces = face_cascade_db.detectMultiScale(img_gray, 1.1, 19)
 
     for (x,y,w,h) in faces: 
