6dd339f82066fdbb5f330ba82b23af6fa696f54f
Gulyutin <gulyutin@gmail.com>
2023-02-15 17:18:52 +0700

    Webcam Eye recognition from UAV video stream updated

--- OpenCV_EyeRecognition_webcam/main.py
+++ OpenCV_EyeRecognition_webcam/main.py
@@ -3,11 +3,7 @@ import cv2
 face_cascade_db = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
 eye_cascade_db = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_eye.xml")
 
-myrtmp_addr = "rtmp://ovsu.mycdn.me/input/4728756120437_3756447238773_iwly3g3icu"
-cap = cv2.VideoCapture(myrtmp_addr)
-
-
-# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 while True: 
     success, img = cap.read()
@@ -19,7 +15,7 @@ while True:
     for (x,y,w,h) in faces: 
         cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
         img_gray_face = img_gray[y:y+h,x:x+w]
-        eyes = eye_cascade_db.detectMultiScale(img_gray_face, 1.1, 19)
+        eyes = eye_cascade_db.detectMultiScale(img_gray_face,        1.1, 19)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(img, (x+ex,y+ey), (x+ex+ew, y+ey+eh), (255, 0, 0), 2)
 
@@ -28,4 +24,5 @@ while True:
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
